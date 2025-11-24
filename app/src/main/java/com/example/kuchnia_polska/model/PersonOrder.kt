3877dcb644bbad52b93a25963a2020319dcf8fd6
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-11-24 13:23:25 +0100

    filling up PersonOrder model

--- app/src/main/java/com/example/kuchnia_polska/model/PersonOrder.kt
+++ app/src/main/java/com/example/kuchnia_polska/model/PersonOrder.kt
@@ -1,4 +1,9 @@
-package com.example.kuchnia_polska.model
+package com.example.polskakuchnia.model
 
-class PersonOrder {
-}
+data class PersonOrder(
+    var osobaNazwa: String = "",
+    var zupa: String? = null,
+    var danieGl: String? = null,
+    var napoj: String? = null,
+    var cena: Double = 0.0
+)
