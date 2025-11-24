7e64ea68d30d697af60569965945fa368e260778
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-11-24 14:05:05 +0100

    adding bottom nav menu

--- app/src/main/java/com/example/kuchnia_polska/model/Order.kt
+++ app/src/main/java/com/example/kuchnia_polska/model/Order.kt
@@ -4,9 +4,10 @@ package com.example.kuchnia_polska.model
 
 data class Order (
     val zamowienia: MutableList<PersonOrder> = mutableListOf()
-){
+)
+{
 fun add(personOrder: PersonOrder) {
     zamowienia.add(personOrder)
 }
-    fun totalAll(): Double = zamowienia.sumOf { it.cena }
+    fun totalAll(): Double = zamowienia.sumOf {it.cena}
 }
