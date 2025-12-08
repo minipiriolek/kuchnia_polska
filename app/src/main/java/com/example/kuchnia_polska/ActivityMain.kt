89405abffe641b389b8b8de7f2738d19253ebbcf
olo <aleksander.malicki@cyfroweszkoly.pl>
2025-12-08 13:06:14 +0100

    bottom nav menu update and activity main filled up

---
+++ app/src/main/java/com/example/kuchnia_polska/ActivityMain.kt
@@ -0,0 +1,20 @@
+package com.example.kuchnia_polska
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.setupWithNavController(navController)
+    }
+}
--- app/src/main/java/com/example/kuchnia_polska/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.kuchnia_polska;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-}
